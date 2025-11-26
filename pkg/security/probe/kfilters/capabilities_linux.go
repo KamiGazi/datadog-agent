dbf1853c34f2625e2b74b0057d2b4092956db6b4
Lorenzo Susini <lorenzo.susini@datadoghq.com>
2025-11-26 13:05:58 +0100

    feat: add socket event approvers and kfilters

--- pkg/security/probe/kfilters/capabilities_linux.go
+++ pkg/security/probe/kfilters/capabilities_linux.go
@@ -105,4 +105,5 @@ func init() {
 	allCapabilities["connect"] = connectCapabilities
 	allCapabilities["prctl"] = prctlCapabilities
 	allCapabilities["setsockopt"] = setsockoptCapabilities
+	allCapabilities["socket"] = socketCapabilities
 }
