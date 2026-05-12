a3ce45d6c814da51a7af5f4b3b04419836c059c8
louis-cqrl <louis.coquerelle@datadoghq.com>
2026-05-12 15:57:25 +0000

    refactor(core): migrate comp/core/flare to V2 component architecture

--- cmd/agent/subcommands/run/command_windows.go
+++ cmd/agent/subcommands/run/command_windows.go
@@ -49,7 +49,7 @@ import (
 	agenttelemetry "github.com/DataDog/datadog-agent/comp/core/agenttelemetry/def"
 	"github.com/DataDog/datadog-agent/comp/core/autodiscovery"
 	"github.com/DataDog/datadog-agent/comp/core/config"
-	"github.com/DataDog/datadog-agent/comp/core/flare"
+	flaredef "github.com/DataDog/datadog-agent/comp/core/flare/def"
 	"github.com/DataDog/datadog-agent/comp/core/gui"
 	"github.com/DataDog/datadog-agent/comp/core/hostname/hostnameinterface"
 	ipc "github.com/DataDog/datadog-agent/comp/core/ipc/def"
@@ -107,7 +107,7 @@ func StartAgentWithDefaults(ctxChan <-chan context.Context) (<-chan error, error
 		err := fxutil.OneShot(func(
 			log log.Component,
 			config config.Component,
-			flare flare.Component,
+			flare flaredef.Component,
 			telemetry telemetry.Component,
 			sysprobeConf sysprobeconfig.Component,
 			server dogstatsdServer.Component,
