8e667e317bf4bfadad68cc57fa4c4634e5662f07
Kevin Fairise <kevin.fairise@datadoghq.com>
2026-05-22 21:56:04 +0200

    Remove changes on agentOptions

--- test/e2e-framework/scenarios/aws/kindvm/run_args.go
+++ test/e2e-framework/scenarios/aws/kindvm/run_args.go
@@ -56,7 +56,7 @@ func GetRunParams(opts ...RunOption) *RunParams {
 	p := &RunParams{
 		Name:                defaultKindName,
 		vmOptions:           []ec2.VMOption{},
-		agentOptions:        []kubernetesagentparams.Option{},
+		agentOptions:        nil, // nil by default - Agent is only deployed when options are explicitly provided
 		fakeintakeOptions:   []fakeintake.Option{},
 		workloadAppFuncs:    []kubecomp.WorkloadAppFunc{},
 		depWorkloadAppFuncs: []kubecomp.AgentDependentWorkloadAppFunc{},
@@ -129,10 +129,6 @@ func WithFakeintakeOptions(opts ...fakeintake.Option) RunOption {
 // WithoutFakeIntake disables fakeintake creation
 func WithoutFakeIntake() RunOption {
 	return func(p *RunParams) error { p.fakeintakeOptions = nil; return nil }
-}
-
-func WithoutAgent() RunOption {
-	return func(p *RunParams) error { p.agentOptions = nil; return nil }
 }
 
 // WithCiliumOptions sets cilium options
